b23a5ff5268c83ea86e89b04573738d9457772c3
KawalaE <edyta.wer.kawala@gmail.com>
2024-06-12 11:11:05 +0200

    Add stale time property to genres fetch hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,14 @@
 import APIClient from "../services/api-client";
 import Genre from "../entities/Genre";
 import { useQuery } from "@tanstack/react-query";
-
+import ms from "ms";
 const newAPIClient = new APIClient<Genre>("/genres/anime");
 
 const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
     queryFn: () => newAPIClient.getAll(),
+    staleTime: ms("24h"),
   });
 };
 export default useGenres;
