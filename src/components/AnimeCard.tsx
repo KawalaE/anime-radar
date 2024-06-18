a042439a17f4d837c7110bbb030cd44bd65048b4
KawalaE <edyta.wer.kawala@gmail.com>
2024-06-18 11:46:10 +0200

    Add a reverse working side to the card component

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -21,83 +21,96 @@ interface Props {
 }
 
 const AnimeCard = ({ animeInfo: anime }: Props) => {
-  const [isHovering, setIsHovering] = useState(false);
-  const titleLength = anime.title_english
-    ? anime.title_english.length
-    : anime.title.length;
-  console.log(titleLength);
-  const offset = titleLength - 15 > 0 ? titleLength : 0;
+  const mainTitle = anime.title_english ? anime.title_english : anime.title;
+  const shortendTitle =
+    mainTitle.length > 30 ? mainTitle.substring(0, 40) + "..." : mainTitle;
   const animeDescription = anime.synopsis
-    ? anime.synopsis.substring(0, 140 - offset) + "..."
+    ? anime.synopsis.substring(0, 120) + "..."
     : "Description unavailable :(";
   return (
     <Card
       w={[325, 270, 250, 260, 220]}
       h={[590, 500, 470, 490, 430]}
       overflow="hidden"
-      onPointerOver={() => setIsHovering(true)}
-      onPointerLeave={() => setIsHovering(false)}
+      role="group"
     >
-      {isHovering && (
-        <>
-          <CardBody
-            display="flex"
-            flexDir="column"
-            overflow="hidden"
-            justifyContent="space-around"
-          >
-            <Heading size="sm" color="teal.400" mt={3}>
-              {anime.title_english ? anime.title_english : anime.title}
-            </Heading>
+      {/**Main card body */}
+      <CardBody
+        padding={0}
+        _groupHover={{ display: "none" }}
+        display="flex"
+        flexDir="column"
+        overflow="hidden"
+        justifyContent="space-between"
+      >
+        <AspectRatio ratio={2 / 3} alignContent="flex-start">
+          <Image
+            objectFit="cover"
+            borderTopRadius={5}
+            src={anime.images.webp.large_image_url}
+          ></Image>
+        </AspectRatio>
+        <VStack alignItems="flex-start" ms={3}>
+          <Heading size="sm" color="teal.400" mr={3} mt={3}>
+            {shortendTitle}
+          </Heading>
+          <HStack>
+            <Text>{anime.score}</Text>
+            <StarIcon
+              color={anime.score > 8 ? "yellow.400" : undefined}
+              mb="1rem"
+            />
+            <Text> ({convertVotes(anime.scored_by)})</Text>
+          </HStack>
+        </VStack>
+      </CardBody>
+      {/**Reverse card body */}
+      <CardBody
+        pos="relative"
+        display="none"
+        _before={{
+          content: '""',
+          bgImage: `url(${anime.images.webp.large_image_url})`,
+          backgroundSize: "cover",
+          position: "absolute",
+          top: "0px",
+          right: "0px",
+          bottom: " 0px",
+          left: "0px",
+          opacity: "0.3",
+          filter: "brightness(0.25) ",
+        }}
+        _groupHover={{
+          display: "flex",
+          flexDir: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <Heading position="relative" size="sm">
+          {shortendTitle}
+        </Heading>
+        <VStack position="relative" alignItems="flex-start">
+          <HStack>
+            <Text>{anime.score}</Text>
+            <StarIcon
+              color={anime.score > 8 ? "yellow.400" : undefined}
+              mb="1rem"
+            />
+            <Text> ({convertVotes(anime.scored_by)})</Text>
+          </HStack>
+          <Text position="relative" color="gray.400" m="0" p="0">
+            Episodes: {anime.episodes}
+          </Text>
+          <Text position="relative" color="gray.400" m="0" p="0">
+            Status: {anime.status}
+          </Text>
+        </VStack>
 
-            <VStack alignItems="flex-start">
-              <HStack>
-                <Text>{anime.score}</Text>
-                <StarIcon
-                  color={anime.score > 8 ? "yellow.400" : undefined}
-                  mb="1rem"
-                />
-                <Text> ({convertVotes(anime.scored_by)})</Text>
-              </HStack>
-              <Text color="gray.500" m="0" p="0">
-                Episodes: {anime.episodes}
-              </Text>
-              <Text color="gray.500" m="0" p="0">
-                Status: {anime.status}
-              </Text>
-            </VStack>
-
-            <Text>{animeDescription}</Text>
-            <Button alignSelf="flex-end" colorScheme="teal">
-              More info
-            </Button>
-          </CardBody>
-        </>
-      )}
-      {isHovering == false && (
-        <>
-          <AspectRatio ratio={2 / 3}>
-            <Image
-              borderTopRadius={5}
-              src={anime.images.webp.large_image_url}
-            ></Image>
-          </AspectRatio>
-
-          <CardBody>
-            <Heading size="sm">
-              {anime.title_english ? anime.title_english : anime.title}
-            </Heading>
-            <HStack>
-              <Text>{anime.score}</Text>
-              <StarIcon
-                color={anime.score > 8 ? "yellow.400" : undefined}
-                mb="1rem"
-              />
-              <Text> ({convertVotes(anime.scored_by)})</Text>
-            </HStack>
-          </CardBody>
-        </>
-      )}
+        <Text position="relative">{animeDescription}</Text>
+        <Button alignSelf="flex-end" colorScheme="teal">
+          More info
+        </Button>
+      </CardBody>
     </Card>
   );
 };
