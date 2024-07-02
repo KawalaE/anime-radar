0b399f81eeb3745d7d6757a2ae5c6426b5f17ab9
KawalaE <edyta.wer.kawala@gmail.com>
2024-07-02 12:19:06 +0200

    Create a component with the not found content

--- src/components/AnimeGrid.tsx
+++ src/components/AnimeGrid.tsx
@@ -8,32 +8,15 @@ import {
 } from "@chakra-ui/react";
 import AnimeCard from "./AnimeCard";
 import { Anime } from "../entities/Anime";
-import useAnimeQueryStore from "../store";
+import AnimeNotFound from "./AnimeNotFound";
 
 interface Props {
   animes?: Anime[];
 }
 const AnimeGrid = ({ animes }: Props) => {
-  const resetAllSearches = useAnimeQueryStore((s) => s.resetAll);
   return (
     <>
-      {!animes?.length && (
-        <Flex marginTop={20} flexDir="column" alignItems="center">
-          <Image
-            boxSize={["250px", "350px"]}
-            src={`src/assets/anime-found-${Math.floor(Math.random() * 3)}.gif`}
-          ></Image>
-          <Heading size="lg" textAlign="center" mt={10}>
-            Oops... No anime was found.
-          </Heading>
-          <Heading size="md" textAlign="center">
-            Looks like it's hiding in another dimension!
-          </Heading>
-          <Button onClick={resetAllSearches} colorScheme="teal" variant="ghost">
-            Reset my filters!
-          </Button>
-        </Flex>
-      )}
+      {!animes?.length && <AnimeNotFound />}
       <SimpleGrid
         columns={{ sm: 2, md: 3, lg: 4, "2xl": 6 }}
         spacing={10}
---
+++ src/components/AnimeNotFound.tsx
@@ -0,0 +1,26 @@
+import { Flex, Heading, Button, Image } from "@chakra-ui/react";
+import React from "react";
+import useAnimeQueryStore from "../store";
+
+const AnimeNotFound = () => {
+  const resetAllSearches = useAnimeQueryStore((s) => s.resetAll);
+  return (
+    <Flex marginTop={20} flexDir="column" alignItems="center">
+      <Image
+        boxSize={["250px", "350px"]}
+        src={`src/assets/anime-found-${Math.floor(Math.random() * 3)}.gif`}
+      ></Image>
+      <Heading size="lg" textAlign="center" mt={10}>
+        Oops... No anime was found.
+      </Heading>
+      <Heading size="md" textAlign="center">
+        Looks like it's hiding in another dimension!
+      </Heading>
+      <Button onClick={resetAllSearches} colorScheme="teal" variant="ghost">
+        Reset my filters!
+      </Button>
+    </Flex>
+  );
+};
+
+export default AnimeNotFound;
