78d434f85b0d1a5b441546236eaa5de8acf0cf47
KawalaE <edyta.wer.kawala@gmail.com>
2024-08-05 11:08:14 +0200

    Refactor order selector

---
+++ src/components/DefaultSelector.tsx
@@ -0,0 +1,47 @@
+import { CheckIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { refactorName } from "./UtilityFunctions";
+
+interface Props {
+  selectionItems: string[];
+  tagName: string;
+  setSelectedValue: (element: string) => void;
+  currentSelectedValue: string | undefined;
+}
+const DefaultSelector = ({
+  selectionItems,
+  tagName,
+  setSelectedValue,
+  currentSelectedValue,
+}: Props) => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+        {tagName}
+      </MenuButton>
+      <MenuList>
+        {selectionItems.map((item) => (
+          <MenuItem
+            key={item}
+            value={item}
+            flexDir="row-reverse"
+            icon={
+              currentSelectedValue === item ? (
+                <CheckIcon color="teal.400" />
+              ) : (
+                <></>
+              )
+            }
+            onClick={() => {
+              setSelectedValue(item);
+            }}
+          >
+            {refactorName(item)}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default DefaultSelector;
--- src/components/OrderSelector.tsx
+++ src/components/OrderSelector.tsx
@@ -1,40 +1,16 @@
-import { CheckIcon, ChevronDownIcon } from "@chakra-ui/icons";
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import useAnimeQueryStore from "../store";
-import { refactorName } from "./UtilityFunctions";
+import DefaultSelector from "./DefaultSelector";
 
 const OrderSelector = () => {
   const setOrderBy = useAnimeQueryStore((s) => s.setOrderBy);
   const selectedOrder = useAnimeQueryStore((s) => s.animeQuery.orderBy);
-  const categoriesOfOrder = ["popularity", "score", "favorites", "scored_by"];
-
   return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-        {"Order by"}
-      </MenuButton>
-      <MenuList>
-        {categoriesOfOrder.map((category) => (
-          <MenuItem
-            key={category}
-            value={category}
-            flexDir="row-reverse"
-            icon={
-              selectedOrder === category ? (
-                <CheckIcon color="teal.400" />
-              ) : (
-                <></>
-              )
-            }
-            onClick={() => {
-              setOrderBy(category);
-            }}
-          >
-            {refactorName(category)}
-          </MenuItem>
-        ))}
-      </MenuList>
-    </Menu>
+    <DefaultSelector
+      selectionItems={["popularity", "score", "favorites", "scored_by"]}
+      tagName={"Order by"}
+      setSelectedValue={setOrderBy}
+      currentSelectedValue={selectedOrder}
+    />
   );
 };
 
