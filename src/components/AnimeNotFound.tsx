156a35f519bed4953333aa0704b400f9c7611498
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-22 12:32:05 +0200

    Fix anime not found issue

--- src/components/AnimeNotFound.tsx
+++ src/components/AnimeNotFound.tsx
@@ -7,7 +7,7 @@ const AnimeNotFound = () => {
     <Flex marginTop={20} flexDir="column" alignItems="center">
       <Image
         boxSize={["250px", "350px"]}
-        src={`src/assets/anime-found-${Math.floor(Math.random() * 3)}.gif`}
+        src={"../src/assets/anime-found-1.gif"}
       ></Image>
       <Heading size="lg" textAlign="center" mt={10}>
         Oops... No anime was found.
