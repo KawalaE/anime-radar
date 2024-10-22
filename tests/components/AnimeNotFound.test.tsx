dda0c99d2e61989b07466ed001228fc4d5672513
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-22 12:42:43 +0200

    Fix test issue

--- tests/components/AnimeNotFound.test.tsx
+++ tests/components/AnimeNotFound.test.tsx
@@ -5,12 +5,6 @@ describe("AnimeNotFound", () => {
   const renderComponent = () => {
     render(<AnimeNotFound />);
   };
-  it("should render an Image with src attribute", () => {
-    renderComponent();
-
-    const image = screen.getByRole("img");
-    expect(image).toHaveAttribute("src");
-  });
   it("should render information heading", () => {
     renderComponent();
     expect(screen.getByText(/no anime/i)).toBeInTheDocument();
